4716f9c3b31d3b0651d8334b191bb800c724d6a0
Gustavo Oliveira <g.92oliveira@gmail.com>
2021-02-02 15:49:55 -0300

    refactor core

--- lib/decent_app/core.ex
+++ lib/decent_app/core.ex
@@ -1,62 +1,64 @@
 defmodule DecentApp.Core do
   alias DecentApp.Core.Validation
-  def bla(bal, res, command) do
-    is_error = Validation.is_valid?(res, command)
 
-    if is_error do
-      {nil, nil, true}
-    else
-      new_balance = %{bal | coins: bal.coins - 1}
+  defp new_balance(true, _, _, _), do: {nil, nil, true}
 
-      res =
-        cond do
-          command === "NOTHING" ->
-            res
+  defp new_balance(false, bal, command, res) do
+    new_balance = %{bal | coins: bal.coins - 1}
 
-          true ->
-            cond do
-              command == "DUP" ->
-                res ++ [List.last(res)]
+    res =
+      cond do
+        command === "NOTHING" ->
+          res
 
-              true ->
-                if command == "POP" do
-                  {_, res} = List.pop_at(res, length(res) - 1)
-                  res
-                else
-                  cond do
-                    command == "+" ->
-                      [first, second | rest] = Enum.reverse(res)
-                      Enum.reverse(rest) ++ [first + second]
+        true ->
+          cond do
+            command == "DUP" ->
+              res ++ [List.last(res)]
 
-                    command == "-" ->
-                      [first, second | rest] = Enum.reverse(res)
-                      Enum.reverse(rest) ++ [first - second]
+            true ->
+              if command == "POP" do
+                {_, res} = List.pop_at(res, length(res) - 1)
+                res
+              else
+                cond do
+                  command == "+" ->
+                    [first, second | rest] = Enum.reverse(res)
+                    Enum.reverse(rest) ++ [first + second]
 
-                    is_integer(command) ->
-                      res ++ [command]
+                  command == "-" ->
+                    [first, second | rest] = Enum.reverse(res)
+                    Enum.reverse(rest) ++ [first - second]
 
-                    command == "COINS" ->
-                      res
-                  end
+                  is_integer(command) ->
+                    res ++ [command]
+
+                  command == "COINS" ->
+                    res
                 end
-            end
-        end
+              end
+          end
+      end
 
-      new_balance =
-        if command == "COINS" do
-          %{new_balance | coins: new_balance.coins + 6}
-        else
-          new_balance
-        end
+    new_balance =
+      if command == "COINS" do
+        %{new_balance | coins: new_balance.coins + 6}
+      else
+        new_balance
+      end
 
-      new_balance =
-        if command == "+" do
-          %{new_balance | coins: new_balance.coins - 1}
-        else
-          new_balance
-        end
+    new_balance =
+      if command == "+" do
+        %{new_balance | coins: new_balance.coins - 1}
+      else
+        new_balance
+      end
 
-      {new_balance, res, false}
-    end
+    {new_balance, res, false}
+  end
+
+  def bla(bal, res, command) do
+    Validation.is_valid?(res, command)
+    |> new_balance(bal, command, res)
   end
 end
